dd0af424be958d16cc97ba1358f4d7255fe56b84
Senyo Ahadzie <Senyo.E.Ahadzie@rice.edu>
2021-01-13 13:03:24 -0600

    Completed function initializeAllOptions, Macro-Step 1 of the solution procedure

--- assets/js/gamestate.js
+++ assets/js/gamestate.js
@@ -4,28 +4,65 @@
 function initializeAllOptions (sudokuBoard)
   {
     //Initializes an array that fully captures all possible options for the initial configuration of the Sudoku board from the input sudokuBoard array
-    var sudokuBoardAll = sudokuBoard;
+    var sudokuBoardAll = [];
+    for (var i=0;i<9;i++)
+    {
+      var thiszFillArr=[0,0,0,0,0,0,0,0,0];
+      sudokuBoardAll.push(thiszFillArr);
+    }
 
     //Order 3x3 boxes by most filled values
     var BoxFilledPos=[];
+
     // console.log('Works so far');
     for (var i=0;i<9;i++)
     {
         //Get subBox i from sudokuBoard
         var subBox=[];
+        var boxUsedValues=[];
+        var numbers1to9=[1,2,3,4,5,6,7,8,9];
+        var correspondingRows=[3*(Math.floor(i/3)), 1+3*(Math.floor(i/3)), 2+3*(Math.floor(i/3))];
+        var correspondingCols=[3*(i-3*Math.floor((i)/3)), 1+3*(i-3*Math.floor((i)/3)), 2+3*(i-3*Math.floor((i)/3))];
         for (var j=3*(Math.floor(i/3)); j<3+3*(Math.floor(i/3));j++)
         {
             var tempsBArr=[];
             for (var k=3*(i-3*Math.floor((i)/3));k<3+3*(i-3*Math.floor(i/3));k++)
             {
-            tempsBArr.push(sudokuBoard[j][k]);
+                if (sudokuBoard[j][k]!==0)
+                {
+                    tempsBArr.push(sudokuBoard[j][k]);
+                    BoxFilledPos=BoxFilledPos+1;
+                    boxUsedValues.push(sudokuBoard[j][k]);
+                }
+                else
+                {
+                  tempsBArr.push(0);
+                }
+            
             
             }
             subBox.push(tempsBArr);
+        }
+    
+      var numsLeftToPick=setdiff(numbers1to9,boxUsedValues);
+      
+      for (var j=0;j<3;j++)
+      {
+        for (var k=0;k<3;k++)
+        {
+          if (subBox[j][k]==0)
+          {
+            sudokuBoardAll[correspondingRows[j]][correspondingCols[k]]=parseInt(intersect(intersect(setdiff(numbers1to9,sudokuBoard[correspondingRows[j]]),setdiff(numbers1to9,getCol(sudokuBoard,correspondingCols[k]))),numsLeftToPick).join(""));
+          }
+          else
+          {
+            sudokuBoardAll[correspondingRows[j]][correspondingCols[k]]=subBox[j][k];
+          }
+          
+          
+        }
       }
-      console.log(subBox);
     }
-
 
 
     return sudokuBoardAll;
@@ -72,3 +109,44 @@ function hailMaryUnsolvedBoards(boardSolved,sudokuBoardAll)
 // boardSolved = testIsSolution(rowSolved,colSolved,boxSolved,sudokuBoardAll);
 
 // [storeBoards,storeBoardsSolvedMaybe,storeBoardsSolvedVars]=hailMaryUnsolvedBoards(boardSolved,sudokuBoardAll);
+
+function setdiff(a,b)
+{
+    var c=[];
+
+    for (var i=0;i<a.length;i++)
+    {
+        if (!b.includes(a[i]))
+        {
+            c.push(a[i]);
+        }
+    }
+
+    return c;
+}
+
+function intersect(a,b)
+{
+    var c=[];
+
+    for (var i=0;i<b.length;i++)
+    {
+        if (a.includes(b[i]))
+        {
+            c.push(b[i]);
+        }
+    }
+
+    return c;
+}
+
+function getCol(a,colToGet)
+{
+    var colGot=[];
+    for (var i=0;i<a.length;i++)
+    {
+        colGot.push(a[i][colToGet]);
+    }
+
+    return colGot;
+}
