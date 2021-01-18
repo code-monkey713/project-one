1b82faf01d3e29b89c7956e994d17326bfc04146
Tony Tran <codemonkey713@gmail.com>
2021-01-18 13:57:16 -0600

    looking for correct syntax to update just one value in array with certain index

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,132 +3,13 @@ $(document).ready(function () {
   let imgArray = [];
   let imgIndex = 0;
   let currSquare = '';
-  let difficulty = 'easy';
   let easyBoard = [];
   let mediumBoard = [];
   let hardBoard = [];
+  let difficultySelected; 
   let currBoard = [];
   let setTheme = false;
   let setDifficulty = false;
-
-  function getDifficulty(diff) {
-    // AJAX Call to get info from the Sudoku API
-    $.ajax(
-      {
-        url: 'https://sugoku.herokuapp.com/board?difficulty=' + diff,
-        method: 'GET'
-      }
-    ).then(function (response) {
-      console.log(response);
-      // console.log(response.board[0][0]);
-      // console.log(response.board[0].length);
-
-      for (let x = 0; x < response.board.length; x++) {
-        // console.log(response.board[colIndex]);
-        let col = response.board[x];
-        // console.log(col);
-
-        for (let y = 0; y < 9; y++) {
-
-          // console.log(response.board[x][y]);
-          if (x === 0) {
-            $(`#A${y}`).html(col[y]);
-            // console.log(`filled for A${y}`);
-            if (col[y] === 0) {
-              // $(`#A${y}`).css('opacity', 0.0);
-              $(`#A${y}`).html('');
-              $(`#A${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 1) {
-            $(`#B${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#B${y}`).html('');
-              $(`#B${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 2) {
-            $(`#C${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#C${y}`).html('');
-              $(`#C${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 3) {
-            $(`#D${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#D${y}`).html('');
-              $(`#D${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 4) {
-            $(`#E${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#E${y}`).html('');
-              $(`#E${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 5) {
-            $(`#F${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#F${y}`).html('');
-              $(`#F${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 6) {
-            $(`#G${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#G${y}`).html('');
-              $(`#G${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 7) {
-            $(`#H${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#H${y}`).html('');
-              $(`#H${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          } else if (x === 8) {
-            $(`#I${y}`).html(col[y]);
-            if (col[y] === 0) {
-              $(`#I${y}`).html('');
-              $(`#I${y}`).css('background-image', 'url(' + imgArray[imgIndex] + ')');
-              imgIndex++;
-              if (imgIndex > 49) {
-                imgIndex = 0;
-              }
-            }
-          }
-        };
-      };
-    }
-    )
-  };
 
   // function for getting giphy images URL and store it in an array
   function getPix(topic) {
@@ -154,24 +35,27 @@ $(document).ready(function () {
       url: queryURL,
       method: "GET"
     }).then(function (response) {
-      console.log(response);
-      currBoard.push(response.board);
+      // console.log(response);
+      // currBoard.push(response.board);
       if (mode === 'easy') {
+        easyBoard = [];
         easyBoard.push(response.board);
         console.log(easyBoard);
       }
       if (mode === 'medium') {
+        mediumBoard = [];
         mediumBoard.push(response.board);
         console.log(mediumBoard);
       }
       if (mode === 'hard') {
+        hardBoard = [];
         hardBoard.push(response.board);
         console.log(hardBoard);
       };
     });
   };
 
-  // function to render the board from array of numbers
+  // function to render the board from array of numbers passed to
   function renderBoard(arr) {
     console.log(arr);
     for (let x = 0; x < arr[0].length; x++) {
@@ -275,15 +159,34 @@ $(document).ready(function () {
 
   // function to get the value from the playing squared and add the value to the index
   function getState(currBoard,pressedID,valueToChange) {
-    let currBoardExtract=currBoard[0];
-    // console.log('called on function to get current state of board and store to array');
+    console.log(currBoard);
+    console.log(`pressed ID: ${pressedID}; value to change: ${valueToChange}`);
+    let currBoardExtract = []
+    currBoardExtract.push(currBoard[0]);
     console.log(currBoardExtract);
     let alphaArr=['A','B','C','D','E','F','G','H','I'];
-    let rowToBeChanged=alphaArr.indexOf(pressedID[1]);
-    console.log(pressedID);
-    let colToBeChanged=pressedID[0];
-    currBoardExtract[rowToBeChanged][colToBeChanged]=valueToChange;
-    currBoard[0]=currBoardExtract;
+    let split = pressedID.split("");
+    let colValue=split[0];
+    let colToBeChanged=alphaArr.indexOf(colValue);
+    // console.log(colToBeChanged);
+    let rowToBeChanged=parseInt(split[1]);
+    // console.log(rowToBeChanged);
+    // currBoard.splice(rowToBeChanged, colToBeChanged, valueToChange);
+    // currBoard[rowToBeChanged, colToBeChanged] = valueToChange;
+    console.log(currBoard);
+    currBoardExtract.splice([rowToBeChanged][colToBeChanged],1,valueToChange);
+    console.log(currBoardExtract);
+    // currBoardExtract[rowToBeChanged][colToBeChanged]=push(valueToChange);
+    // currBoard = [];
+    // console.log(currBoard);
+    // currBoard.push(currBoardExtract);
+    // console.log(currBoardExtract);
+    // currBoardExtract = [];
+  };
+
+  // function to clear the playing board of all the squares
+  function clearBoard() {
+    console.log('this clears the playing field');
   };
 
   // function to get initial screen up with initial board ready to play
@@ -291,7 +194,6 @@ $(document).ready(function () {
     getGameBoard('easy');
     getGameBoard('medium');
     getGameBoard('hard');
-    // renderBoard(easyBoard);
     // solverFunction(easy);
     // solverFunction(medium);
     // solverFunction(hard);
@@ -308,13 +210,15 @@ $(document).ready(function () {
   $(".fieldBtn").click(function () {
     $(`#${currSquare}`).html($(this).val());
     getState(currBoard,currSquare,parseInt($(this).val()));
-    renderBoard(currBoard[0]);
+    // clearBoard();
+    // renderBoard(currBoard[0]);
   });
 
   $('body').keydown(function (e) {
     $(`#${currSquare}`).html(e.key);
     getState(currBoard,currSquare,parseInt(e.key));
-    renderBoard(currBoard[0]);
+    // clearBoard();
+    // renderBoard(currBoard[0]);
   });
 
   $('.theme').on('click', function () {
@@ -330,13 +234,20 @@ $(document).ready(function () {
   });
 
   $('.difficulty').on('click', function () {
-    let diff = $(this).attr('data-lvl');
-    console.log(diff);
+    difficultySelected = $(this).attr('data-lvl');
     $('.difficulty').hide();
     $('.diff-head').hide();
-    // getDifficulty(diff);
-    currBoard.push(easyBoard);
-    console.log(currBoard);
+    if(difficultySelected === 'easy'){
+      currBoard.push(easyBoard[0]);
+    }
+    if(difficultySelected === 'medium'){
+      currBoard.push(mediumBoard[0]);
+    }
+    if(difficultySelected === 'hard'){
+      currBoard.push(hardBoard[0]);
+    }
+    // console.log(currBoard);
+    renderBoard(currBoard);
     setDifficulty = true;
     if (setTheme === true && setDifficulty === true) {
       $('#reset').removeClass('hide');
